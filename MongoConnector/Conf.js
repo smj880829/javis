a6348a31a16e12c501472fac0acf2fe7be62f2ba
unknown <>
2016-07-30 14:48:10 +0900

    asd

---
+++ MongoConnector/Conf.js
@@ -0,0 +1,7 @@
+var ip = "54.199.240.31"
+var port = 27017
+var db = "single_chat"
+var col = "chat_log"
+var url = "mongodb://" + ip + ":" + port + "/" + db
+exports.url = url
+exports.col = col
---
+++ mysocketio.js
@@ -0,0 +1,38 @@
+var io =  require('socket.io')();
+var db = require('./MongoConnector/DAO')
+var conf = require('./MongoConnector/Conf')
+var fw = require('./findWord')
+
+module.exports = function(options) {
+  io.attach(options,{origins:conf.ip +':* http://' + conf.ip +':*'});
+  //io.attach(options);
+};
+
+io.on('connection', function (socket) {
+    console.log('socket connect');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('insert_chatlog', function(data){
+    data.insert_time = new Date()
+    db.insert(data,function(re){
+    })
+
+    socket.broadcast.emit('new_chat_log', data);
+  })
+
+  socket.on('init_chat_log', function(data){
+    db.find_sort_limit({},{'insert_time':-1},15,function(re){
+      socket.emit('chat_logs', re);
+    })
+  });
+
+  socket.on('find_chatlog', function(data){
+    db.find_sort_limit({},{'insert_time':-1},15,function(re){
+      socket.emit('chat_logs', re);
+    })
+  })
+
+})
