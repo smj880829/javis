514e2c22bae444190a99cb79bda7dc9e5e96d348
unknown <>
2016-07-31 19:14:38 +0900

    asd

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,14 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: 'welcome' });
 });
 
+router.post('/login', function(req, res, next) {
+  if(req.body.email == '123' &&  req.body.password == '123'){
+    res.redirect('/');
+  }else {
+    res.render('login',{title:'잘못된 계정입니다 ㅠ.'});
+  }
+});
+
 router.get('/logout', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
