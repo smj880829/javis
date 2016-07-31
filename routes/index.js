d5f59f70fab604b3d8f5c7d62ef25999789cbddc
unknown <>
2016-07-31 19:48:10 +0900

    asd

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var db = require('./MongoConnector/DAO')
-var conf = require('./MongoConnector/Conf')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
