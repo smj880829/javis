98fc54a4242189acc6d196739d20e503bb9394d8
unknown <>
2016-07-31 14:02:50 +0900

    asd

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Express' });
 });
 
 router.get('/logout', function(req, res, next) {
