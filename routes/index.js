8acfbb0b2db5499662421f812c6447448335e991
unknown <>
2016-08-01 18:21:00 +0900

    asd

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,11 @@ var auth = function(req, res, next) {
   var check = false
   var url = 'https://graph.facebook.com/debug_token?input_token='+req.body.accessToken+'&access_token=706997686105976|0OZJHFqBqsK_7aGn_Mw_3ETQ2dM'
   http2.get(url, (res) => {
-    console.log(res.data);
-    check = res.data.is_valid
 
+    res.data..is_valid
+    res.on('data', function (chunk) {
+         console.log(chunk);
+       });
   }).on('error', (e) => {
     console.log(`Got error: ${e.message}`);
   });
