4ea5017cdeac4c55a4d3113c487baf30204a0550
unknown <>
2016-08-01 18:23:03 +0900

    asd

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ var auth = function(req, res, next) {
   var check = false
   var url = 'https://graph.facebook.com/debug_token?input_token='+req.body.accessToken+'&access_token=706997686105976|0OZJHFqBqsK_7aGn_Mw_3ETQ2dM'
   http2.get(url, (res) => {
+    res.setEncoding('utf8');
     res.on('data', function (chunk) {
          console.log(chunk);
        });
