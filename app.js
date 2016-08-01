998ade5b5b60a87fb6be6986976f6a6ad975853c
unknown <>
2016-08-01 17:44:38 +0900

    asd

--- app.js
+++ app.js
@@ -75,7 +75,8 @@ app.use(function(err, req, res, next) {
 
 http.listen(80);
 
-http.get('https://graph.facebook.com/oauth/access_token?client_id=706997686105976&client_secret=a0c72b5d0c9152bcd5a8fb0de44435b5&grant_type=client_credentials', (res) => {
+var http2 = require('http')
+http2.get('https://graph.facebook.com/oauth/access_token?client_id=706997686105976&client_secret=a0c72b5d0c9152bcd5a8fb0de44435b5&grant_type=client_credentials', (res) => {
   console.log(res.app-access-token);
 }).on('error', (e) => {
   console.log(`Got error: ${e.message}`);
