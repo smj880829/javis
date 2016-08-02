5a079a97d94480d97aab7cc303d55f95ddacde39
unknown <>
2016-08-02 15:51:12 +0900

    asd

--- public/javascripts/mainApp.js
+++ public/javascripts/mainApp.js
@@ -106,11 +106,11 @@ app.controller('chatCtl',['$scope', '$window','$http','socket','$log','$anchorSc
     function statusChangeCallback(response) {
       console.log(response)
       if (response.status === 'connected') {
-        //$window.location.href = "/";
+        $rootScope.$emit('log_out',{})
       } else if (response.status === 'not_authorized') {
-
+        $rootScope.$emit('log_in',{})
       } else {
-
+        $rootScope.$emit('log_in',{})
       }
     }
 
