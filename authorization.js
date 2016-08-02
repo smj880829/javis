2a9f55255aef4744ab39ae1ae123cc149161d462
unknown <>
2016-08-02 14:37:43 +0900

    asd

--- authorization.js
+++ authorization.js
@@ -21,9 +21,10 @@ get_app_access_token();
 function check_accessToken(token, callback) {
   var https = require('https')
   var url2= 'https://graph.facebook.com/debug_token?input_token='+token+'&access_token=706997686105976|0OZJHFqBqsK_7aGn_Mw_3ETQ2dM'
-  https.get(url2, (re) => {
-    re.setEncoding('utf8');
-    re.on('data', (chunk) => {
+  https.get(url2, (res) => {
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log('data');
         var temp = JSON.parse(chunk)
         callback(temp.data.is_valid)
     }).on('error', (e) => {
