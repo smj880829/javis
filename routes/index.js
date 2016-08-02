d4170b1556a2ecfb34cd60b88f836f7ea8364add
unknown <>
2016-08-02 15:23:06 +0900

    asd

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var auth = require('../authorization')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //req.headers.authorization
-    res.render('index',{login:'LOG IN'});
+    res.render('index');
 });
 
 router.post('/' , function(req, res, next) {
