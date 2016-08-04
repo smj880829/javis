d89e20088e110d93f7a4f827bdfcacbc9d4d89a0
unknown <>
2016-08-04 16:55:29 +0900

    asd

--- public/javascripts/loginApp.js
+++ public/javascripts/loginApp.js
@@ -10,6 +10,8 @@ app.controller('loginCtl',['$scope', '$window','$rootScope','$document','$http',
              $http.defaults.headers.common.Authorization = accessToken;
              $http.defaults.headers.common.loginMethod = 'facebook';
              $window.alert(accessToken)
+
+             $document.getElementById('loginform').submit()
       } else {
 
       }
