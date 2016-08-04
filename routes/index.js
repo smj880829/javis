19d4f2e30bb7177b6e87bd925cbbc5e14c345924
unknown <>
2016-08-04 17:37:13 +0900

    asd

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ router.get('/test', function(req, res, next) {
         res.render('test');
     }
     else {
-      res.redirect('login_error');
+      res.render('login_error');
     }
   })
 });
