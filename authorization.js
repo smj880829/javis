8abf8a8a9ceec25528a59783750484237716fafc
unknown <>
2016-08-05 17:58:20 +0900

    asd

--- authorization.js
+++ authorization.js
@@ -42,9 +42,7 @@ var authorization = function(meth) {        //생성자 선언
 
 }
 
-
-
-function check_user(callback){
+authorization.prototype.check_user = function(callback){
   console.log(this.method + this.email)
   var check = false;
   switch (this.method) {
@@ -52,7 +50,7 @@ function check_user(callback){
                  break;
     case 'facebook'   : check_accessToken_fb(this.externaltoken,function(re){ check = re })
                  break;
-    default    : 
+    default    :
                  break;
   }
 
