bffb42a0c07c3688525761197322e03fadf9b8d6
unknown <>
2016-08-05 20:58:28 +0900

    asd

--- public/javascripts/mainApp.js
+++ public/javascripts/mainApp.js
@@ -1,7 +1,6 @@
 var app = angular.module('mainApp',  ['ngRoute','ngAnimate','ngScrollable'])
 
 app.config(['$routeProvider','$locationProvider',function ($routeProvider,$locationProvider) {
-  $locationProvider.html5Mode(true);
 //Module의 config API를 사용하면 서비스 제공자provider에 접근할 수 있다. 여기선 $route 서비스 제공자를 인자로 받아온다.
  $routeProvider
 //$routeProvider의 when 메소드를 이용하면 특정 URL에 해당하는 라우트를 설정한다. 이때 라우트 설정객체를 전달하는데 <ng-view>태그에 삽입할 탬플릿에 해당하는 url을 설정객체의 templateUrl 속성으로 정의한다.
