b91d5b1e7da34f224930ad3e5be8e60654cc9a4d
unknown <>
2016-08-05 18:01:54 +0900

    asd

--- authorization.js
+++ authorization.js
@@ -114,6 +114,7 @@ function check_accessToken_fb(token, callback) {
     res.setEncoding('utf8');
     res.on('data', (chunk) => {
         var temp = JSON.parse(chunk)
+        console.log(temp)
         callback(temp.data.is_valid)
     }).on('error', (e) => {
       console.log(`auth error`);
