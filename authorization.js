9011fdb9b352dad140d8aa7477782c81641e7b27
unknown <>
2016-08-05 18:16:04 +0900

    asd

--- authorization.js
+++ authorization.js
@@ -77,7 +77,7 @@ function getLocalToken(callback){
   var min = date.getMinutes();
   date.setMinutes(min + 1);
   var payload = { 'email' : this.email, 'expiration':date };
-
+  console.log(this.secret)
   var token = jwt.encode(payload, this.secret);
   callback(token)
 }
