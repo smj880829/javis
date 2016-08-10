0f5e18e1e724dcf4f7e52a74a7f3a0ecd630fcb1
unknown <>
2016-08-10 15:26:31 +0900

    asd

---
+++ public/javascripts/mainChat.js
@@ -0,0 +1,1 @@
+var app = angular.module('mainChat',  ['ngRoute','ngAnimate','ngScrollable','ngCookies'])
---
+++ routes/chat.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var token_ctl = require('../controller_token')
+
+
+function authChecker(req, res, next) {
+  if(req.cookies.token != null){
+        token_ctl.checkToken(req.cookies.token,function(re){
+          if(re){
+            next();
+          }else{
+            res.redirect('/login')
+          }
+        })
+  }else {
+        res.redirect('/login')
+  }
+
+}
+
+
+/* GET home page. */
+router.get('/',authChecker, function(req, res, next) {
+  //req.headers.authorization
+    res.render('index');
+});
+
+
+module.exports = router;
