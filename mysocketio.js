bf2705c734cfed004fc1e14596333b34e75762c5
unknown <>
2016-08-11 01:55:28 +0900

    sad

--- mysocketio.js
+++ mysocketio.js
@@ -52,7 +52,7 @@ io.on('connection', function (socket) {
   //chat
   socket.on('initRoomList', function(data){
     db.find('main_chat',{'type':'roomlist'},function(re){
-      console.log(re);
+      console.log(re.list);
       socket.emit('initRoomList', re);
     })
   })
