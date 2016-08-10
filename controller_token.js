cfc4ed55ec18766e7724187c5a3f9d98fb28710e
unknown <>
2016-08-10 17:46:16 +0900

    asd

--- controller_token.js
+++ controller_token.js
@@ -1,11 +1,12 @@
 var jwt = require('jwt-simple');
 
 var secret = 'wow'
+var connectionTime = 30;
 
 exports.getNewToken = function(callback){
   var date = new Date();
   var min = date.getMinutes();
-  date.setMinutes(min + 1);
+  date.setMinutes(min + connectionTime);
   var payload = { 'email' : this.email, 'year':date.getFullYear(),'month':date.getMonth(),'date':date.getDate(),'hour':date.getHours(),'minutes':date.getMinutes(),'sec':date.getSeconds() };
   var token = jwt.encode(payload, secret);
   callback(token)
@@ -17,8 +18,7 @@ exports.checkToken = function(token,callback){
   var decoded = jwt.decode(token, secret);
   var date2 = new Date(decoded.year,decoded.month,decoded.date,decoded.hour,decoded.minutes,decoded.sec);
     //console.log(decoded); //=> { foo: 'bar' }
-    console.log(date)
-    console.log(date2)
+
     if(date.getTime() < date2.getTime()){
       callback(true)
     }else{
